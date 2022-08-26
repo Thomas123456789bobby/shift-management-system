be0e5e208c29e49e70e8c14e803a3b606d19db58
thomas <46562812+Thomas123456789bobby@users.noreply.github.com>
2022-08-26 22:18:18 +0200

    ww

--- index.php
+++ index.php
@@ -1,30 +1,13 @@
 <?php include('php/include/header.php'); ?>
-<?php
-?>
-<div class="Chatbox-group">
-        <div class="login">
-            <div class="user">
-                <form method="post" action="/php/ChatBox/UserChatBoxsetup.php">
-                    <br><br>
-                    <input type="text" name="uname" placeholder="Email">
-                    <br><br>
-                    <input type="text" name="password" placeholder="password">
-                    <br><br>
-                    <input type="submit" name="chatboxUserVerstuur" value="Verstuur">
-                </form>
-            </div>
-        </div>
-        <div class="login">
-            <div class="admin">
-                <form method="post" action="/php/ChatBox/AdminLogin.php">
-                    <br><br><br>
-                    <input type="text" name="uname" placeholder="Email">
-                    <br><br>
-                    <input type="password" name="password" placeholder="password">
-                    <br><br>
-                    <input type="submit" name="chatboxAdminVerstuur" value="Verstuur">
-                </form>
-            </div>
-        </div>
-</div>
+<div class="main">
+    <p class="sign" align="center">Sign in</p>
+    <form class="form" method="post"  action="/php/login/userlogin.php">
+        <input class="un " type="text" align="center" placeholder="Username">
+        <input class="pass" type="password" align="center" placeholder="Password">
+        <input type="submit" class="submit" align="center">
+    </form>
+      <p class="forgot" align="center"><a href="#">Forgot Password?</p>
+            
+                
+    </div>
 <?php include('php/include/footer.php'); ?>
